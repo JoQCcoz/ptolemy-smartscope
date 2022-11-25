ed561ac3b693e197571c296ec45e61b3dcc8deab
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2022-11-25 13:38:58 -0500

    finalized the hole_finder

--- smartscope_plugin/plugin.py
+++ smartscope_plugin/plugin.py
@@ -1,26 +1,22 @@
 from Smartscope.lib.Datatypes.base_plugin import Finder, TargetClass
-from typing import Optional, Dict, Any
-import sys
+from Smartscope.lib.montage import create_targets_from_center, Target
+from typing import Optional, Dict, Any, List, Union, Tuple
 from pathlib import Path
+import numpy as np
 
 
-class PtolemyPlugin(Finder):
-    # importPaths: Union[str, List] = [os.path.join(__file__,'../')]
+class PtolemyHoleFinder(Finder):
+    description: str = 'Hole finder that uses the ptolemy hole finder to find the holes at medium magnification.'
+    reference: str = 'https://arxiv.org/abs/2112.01534'
     kwargs: Optional[Dict[str, Any]] = { 
         'model_path': Path(__file__).resolve().parents[1] / 'weights/211026_unet_9x64_ep6.torchmodel',
         'cuda': False
     }
 
-    def run(self, *args, **kwargs):
+    def run(self, montage, create_targets_method=create_targets_from_center)-> Tuple[List[Target], bool, Dict]:
         """Where the main logic for the algorithm is"""
-        from .wrapper import ptolemy_find_holes 
-        
-        
-        exposure = ptolemy_find_holes(*args,**kwargs, **self.kwargs)
-        print(exposure.__dict__)
-        # module = importlib.import_module(self.module)
-        # function = getattr(module, self.method)
-        # output = function(*args, **kwargs, **self.kwargs)
-        output = 'hello'
-        print(output)
-        return output
+        from .wrapper import ptolemy_find_holes         
+        exposure = ptolemy_find_holes(montage, **self.kwargs)
+        ptolemy_image_coords = np.array([exposure.crops.center_coords.y*exposure.scale,exposure.crops.center_coords.x*exposure.scale]).transpose()
+        targets = create_targets_method(ptolemy_image_coords,montage)
+        return targets, True, {'lattice_angle': exposure.rot_ang_deg}
