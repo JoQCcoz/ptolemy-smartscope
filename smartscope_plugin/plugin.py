8c15882ca8e1f1c65bb5d2ce0e72bf1f547803f0
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2022-12-01 21:36:58 -0500

    Few adjustments to improve hole finder

--- smartscope_plugin/plugin.py
+++ smartscope_plugin/plugin.py
@@ -10,13 +10,14 @@ class PtolemyHoleFinder(Finder):
     reference: str = 'https://arxiv.org/abs/2112.01534'
     kwargs: Optional[Dict[str, Any]] = { 
         'model_path': Path(__file__).resolve().parents[1] / 'weights/211026_unet_9x64_ep6.torchmodel',
-        'cuda': False
+        'cuda': False,
+        'height': 1024,
     }
 
     def run(self, montage, create_targets_method=create_targets_from_center)-> Tuple[List[Target], bool, Dict]:
         """Where the main logic for the algorithm is"""
         from .wrapper import ptolemy_find_holes         
         exposure = ptolemy_find_holes(montage, **self.kwargs)
-        ptolemy_image_coords = np.array([exposure.crops.center_coords.y*exposure.scale,exposure.crops.center_coords.x*exposure.scale]).transpose()
+        ptolemy_image_coords = np.array([exposure.crops.center_coords.y*exposure.scale,exposure.crops.center_coords.x*exposure.scale],dtype=int).transpose()
         targets = create_targets_method(ptolemy_image_coords,montage)
         return targets, True, {'lattice_angle': exposure.rot_ang_deg}
