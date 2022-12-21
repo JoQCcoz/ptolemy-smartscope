df2b186f5f4c2e3f1a044829da772fe4520f261a
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2022-12-21 15:07:56 -0500

    added support for protocol api

--- smartscope_plugin/wrapper.py
+++ smartscope_plugin/wrapper.py
@@ -8,12 +8,8 @@ import torch
 from Smartscope.lib.image_manipulations import fourier_crop
 
 
-# class SmartScopePtolemy:
-
-
-def ptolemy_find_holes(montage, model_path='weights/211026_unet_9x64_ep6.torchmodel', cuda=True, height=1024) -> Exposure:
-    ex = Exposure(fourier_crop(montage.image,height=height), scale=montage.shape_x/height)
-
+def ptolemy_find_holes(image, model_path='weights/211026_unet_9x64_ep6.torchmodel', cuda=True, height=1024) -> Exposure:
+    ex = Exposure(fourier_crop(image,height=height), scale=image.shape[0]/height)
     segmenter = algorithms.UNet_Segmenter(64, 9, model_path=model_path, dim_mult_of=1024, cuda=cuda)
     ex.make_mask(segmenter)
     processor = algorithms.MedMag_Process_Mask(edge_tolerance=100)
