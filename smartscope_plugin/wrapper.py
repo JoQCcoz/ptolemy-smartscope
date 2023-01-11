debf6f1645aa706da1478603a47430cff5cb5b4a
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2023-01-11 08:49:42 -0500

    added model preloading and auto gpu detection

--- smartscope_plugin/wrapper.py
+++ smartscope_plugin/wrapper.py
@@ -7,10 +7,13 @@ import ptolemy.models as models
 import torch
 from Smartscope.lib.image_manipulations import fourier_crop
 
+def load_model(model_path, cuda=False):
+    return algorithms.UNet_Segmenter(64, 9, model_path=model_path, dim_mult_of=1024, cuda=cuda)
 
-def ptolemy_find_holes(image, model_path='weights/211026_unet_9x64_ep6.torchmodel', cuda=True, height=1024) -> Exposure:
+def ptolemy_find_holes(image, model_path='weights/211026_unet_9x64_ep6.torchmodel', segmenter=None, cuda=False, height=1024) -> Exposure:
     ex = Exposure(fourier_crop(image,height=height), scale=image.shape[0]/height)
-    segmenter = algorithms.UNet_Segmenter(64, 9, model_path=model_path, dim_mult_of=1024, cuda=cuda)
+    if segmenter is None:
+        segmenter = load_model(model_path,cuda)
     ex.make_mask(segmenter)
     processor = algorithms.MedMag_Process_Mask(edge_tolerance=100)
     ex.process_mask(processor)
