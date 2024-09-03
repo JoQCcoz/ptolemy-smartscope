d3240ed32c9e6a3f6ec815f3530d90d146769437
Jonathan <jonathan.bouvette@nih.gov>
2024-09-03 01:35:44 +0000

    fix error with force_mdoc

--- smartscope_plugin/plugin.py
+++ smartscope_plugin/plugin.py
@@ -8,7 +8,6 @@ import os
 from Smartscope.lib.Datatypes.base_plugin import Finder
 from Smartscope.lib.image.target import Target
 from Smartscope.lib.image.targets import Targets
-from Smartscope.core.settings.worker import FORCE_MDOC_TARGETING
 
 from ..ptolemy.algorithms import BadMedMagError
 from .wrapper import ptolemy_find_holes , load_model
@@ -30,14 +29,14 @@ class PtolemyHoleFinder(Finder):
         exposure = ptolemy_find_holes(image, model_path=self.kwargs['model_path'],segmenter=self.segmenter,cuda=self.kwargs['cuda'],height=self.kwargs['height'])
         return np.array([exposure.crops.center_coords.y*exposure.scale,exposure.crops.center_coords.x*exposure.scale],dtype=int).transpose() , {'lattice_angle': exposure.rot_ang_deg}      
 
-    def run(self, montage, create_targets_method=Targets.create_targets_from_center)-> Tuple[List[Target], bool, Dict]:
+    def run(self, montage, create_targets_method=Targets.create_targets_from_center, force_mdoc=False)-> Tuple[List[Target], bool, Dict]:
         """Where the main logic for the algorithm is"""
         if self.kwargs['preload_weights'] and self.segmenter is None:
             print('loading model on first use')
             self.segmenter = load_model(self.kwargs['model_path'], self.kwargs['cuda'])
         try:
             ptolemy_image_coords, additional_outputs = self.find_holes(montage.image)
-            targets = create_targets_method(ptolemy_image_coords,montage,force_mdoc=FORCE_MDOC_TARGETING)
+            targets = create_targets_method(ptolemy_image_coords,montage,force_mdoc=force_mdoc)
             success = True
         except BadMedMagError as err:
             print(f'Could not find holes. {err}')
