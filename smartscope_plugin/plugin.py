aef998198430865d55124bab300d9b2bb785fe34
Jonathan <jonathan.bouvette@nih.gov>
2024-11-26 18:36:02 +0000

    added kwargs to fix new error with non-existing argument

--- smartscope_plugin/plugin.py
+++ smartscope_plugin/plugin.py
@@ -29,7 +29,7 @@ class PtolemyHoleFinder(Finder):
         exposure = ptolemy_find_holes(image, model_path=self.kwargs['model_path'],segmenter=self.segmenter,cuda=self.kwargs['cuda'],height=self.kwargs['height'])
         return np.array([exposure.crops.center_coords.y*exposure.scale,exposure.crops.center_coords.x*exposure.scale],dtype=int).transpose() , {'lattice_angle': exposure.rot_ang_deg}      
 
-    def run(self, montage, create_targets_method=Targets.create_targets_from_center, force_mdoc=False)-> Tuple[List[Target], bool, Dict]:
+    def run(self, montage, create_targets_method=Targets.create_targets_from_center, force_mdoc=False, **kwargs)-> Tuple[List[Target], bool, Dict]:
         """Where the main logic for the algorithm is"""
         if self.kwargs['preload_weights'] and self.segmenter is None:
             print('loading model on first use')
